4b88819151a56c85b63164f3f43abb12067504d7
Tom Rota <mrtomrota@gmail.com>
2017-07-26 15:19:38 -0400

    removed docker cloud logo

--- www/index.php
+++ www/index.php
@@ -16,7 +16,7 @@
 	</style>
 </head>
 <body>
-	<img id="logo" src="logo.png" />
+	<!-- img id="logo" src="logo.png" /-->
 	<h1>
 		<? /* php echo "Hello ".($_ENV["NAME"]?$_ENV["NAME"]:"world")."!"; */ ?>
 	  <?php echo "Automation for the People"; ?>
